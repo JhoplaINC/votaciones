4371afec51e5483735e2bd5433f5d70487a98ec1
Jhopla <jorge.plaza.gj@gmail.com>
2024-06-27 18:53:25 -0400

    cambio cdn jquery

--- plantilla.php
+++ plantilla.php
@@ -55,7 +55,7 @@
             </ul>
         </div>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
     <script src="assets/js/main.js"></script>
     <script src="https://kit.fontawesome.com/ad2b2855c3.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
